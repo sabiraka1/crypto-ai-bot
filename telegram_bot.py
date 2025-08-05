2724c34c58a254b8f138b709a839f032d8540dca
sabiraka1 <sabiraka1@gmail.com>
2025-08-05 15:09:03 +0300

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,21 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+    try:
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+    except Exception as e:
+        print(f"❌ Telegram Error: {e}")
