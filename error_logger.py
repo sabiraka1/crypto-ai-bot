3d996adc8003ac557fcdf4c661fc6e3f349678ed
sabiraka1 <sabiraka1@gmail.com>
2025-08-07 00:20:06 +0300

    Create error_logger.py

---
+++ error_logger.py
@@ -0,0 +1,18 @@
+# error_logger.py
+
+import pandas as pd
+import os
+from signal_analyzer import explain_signal
+
+ERROR_FILE = "error_signals.csv"
+
+def log_error_signal(row):
+    explanation = explain_signal(row)
+    row["explanation"] = explanation
+
+    df = pd.DataFrame([row])
+
+    if os.path.exists(ERROR_FILE):
+        df.to_csv(ERROR_FILE, mode='a', index=False, header=False)
+    else:
+        df.to_csv(ERROR_FILE, index=False, header=True)
