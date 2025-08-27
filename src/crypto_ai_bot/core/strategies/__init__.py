bd72db9185aab1facc8777ef4eca9adc64ac9004
satis <sabiraka1@gmail.com>
2025-08-27 20:30:02 +0300

    0

--- src/crypto_ai_bot/core/strategies/__init__.py
+++ src/crypto_ai_bot/core/strategies/__init__.py
@@ -1,1 +1,35 @@
+from .base import BaseStrategy, StrategyContext, Decision, DecisionEnum, MarketData
+from .ema_cross import EmaCrossStrategy
+from .rsi_momentum import RSIMomentumStrategy
+from .bollinger_bands import BollingerBandsStrategy
+from .position_sizing import (
+    SizeConstraints,
+    fixed_quote_amount,
+    fixed_fractional,
+    naive_kelly,
+)
+from .exit_policies import (
+    ExitPlan,
+    make_fixed_sl_tp,
+    update_trailing_stop,
+    should_exit,
+)
+
+__all__ = [
+    "BaseStrategy",
+    "StrategyContext",
+    "Decision",
+    "DecisionEnum",
+    "MarketData",
+    "EmaCrossStrategy",
+    "RSIMomentumStrategy",
+    "BollingerBandsStrategy",
+    "SizeConstraints",
+    "fixed_quote_amount",
+    "fixed_fractional",
+    "naive_kelly",
+    "ExitPlan",
+    "make_fixed_sl_tp",
+    "update_trailing_stop",
+    "should_exit",
 ]
