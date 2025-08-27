0092174fe5d44ca2a59b32bae119fbe1175db2f6
satis <sabiraka1@gmail.com>
2025-08-27 18:42:25 +0300

    0

--- scripts/test_architecture.py
+++
@@ -1,5 +0,0 @@
-import sys
-from scripts.arch_check import main as arch_main
-def test_architecture_rules():
-    rc = arch_main()
-    assert rc == 0, "arch_check reported violations (see stdout for details)"
