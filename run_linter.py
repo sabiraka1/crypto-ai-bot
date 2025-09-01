b2bfe2a81b55ed524b6ae5a1bc3e891f4449302d
sabiraka1 <sabiraka1@gmail.com>
2025-09-01 10:32:57 +0300

    0

---
+++ run_linter.py
@@ -0,0 +1,7 @@
+﻿import sys
+import locale
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+sys.stdout.reconfigure(encoding='utf-8')
+sys.stderr.reconfigure(encoding='utf-8')
+from importlinter import cli
+cli.lint_imports(['--config', 'importlinter.ini', '--verbose'])
